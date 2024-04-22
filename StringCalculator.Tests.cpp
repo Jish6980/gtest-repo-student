398c564cefa0d268059f94c6def0044bab589e95
Jish6980 <167761259+Jish6980@users.noreply.github.com>
2024-04-22 16:17:49 +0530

    Update StringCalculator.Tests.cpp
    Given input "0" string, Then 0 is expected.

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -11,3 +11,14 @@ TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
     //Assert
     ASSERT_EQ(actualValue,expectedValue);
 }
+TEST(StringCalculatorTestSuite,GivenZeroIsExpected)
+{
+    //Arrange
+    StringCalculator objUnderTest;
+    string input="0";
+    int expectedValue=0;
+    //Act
+   int actualValue=  objUnderTest.Add(input);
+    //Assert
+    ASSERT_EQ(actualValue,expectedValue);
+}
