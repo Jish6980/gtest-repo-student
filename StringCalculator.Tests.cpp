89c7a2d49ea0581542afe1283ca25cd911ed96f5
Jish6980 <167761259+Jish6980@users.noreply.github.com>
2024-04-22 16:27:46 +0530

    Update StringCalculator.Tests.cpp
    Given inputs "1,2,3" string, Then 6 is expected.
    Given inputs "1\n,2,3" string, Then 6 is expected

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -33,3 +33,40 @@ TEST(StringCalculatorTestSuite,GivenOneIsExpected)
     //Assert
     ASSERT_EQ(actualValue,expectedValue);
 }
+
+TEST(StringCalculatorTestSuite,Given2Numbers)
+{
+    //Arrange
+    StringCalculator objUnderTest;
+    string input="1,2";
+    int expectedValue=3;
+    //Act
+   int actualValue=  objUnderTest.Add(input);
+    //Assert
+    ASSERT_EQ(actualValue,expectedValue);
+}
+
+TEST(StringCalculatorTestSuite,when_passed_multiple_comma_delimited_numbers)
+{
+    //Arrange
+    StringCalculator objUnderTest;
+    string input="1,2,3";
+    int expectedValue=6;
+    //Act
+   int actualValue=  objUnderTest.Add(input);
+    //Assert
+    ASSERT_EQ(actualValue,expectedValue);
+}
+
+TEST(StringCalculatorTestSuite,when_delimited_with_newline_and_comma)
+{
+    //Arrange
+    StringCalculator objUnderTest;
+    string input="1\n2,3";
+    int expectedValue=6;
+    //Act
+   int actualValue=  objUnderTest.Add(input);
+    //Assert
+    ASSERT_EQ(actualValue,expectedValue);
+}
+
